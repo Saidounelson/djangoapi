dc38a47342d27d2ab64dd56fc67d401e0c205e52
saidou NASSUFOU <saidou@saidous-MacBook-Pro.local>
2020-02-14 05:05:20 +0000

    return list cours

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
@@ -121,4 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-REST_FRAMEWORK = { 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema' }
+REST_FRAMEWORK = {'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'}
--- djangoapi/urls.py
+++ djangoapi/urls.py
@@ -13,8 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
+from django.conf.urls import url, include
 from django.contrib import admin
+
+admin.site.site_header = "Bienvenu"
+admin.site.site_title = "Sous titre"
+admin.site.index_title = "login"
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
