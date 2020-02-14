a380d713af41a6859bc739798794c67bb7d92546
saidou NASSUFOU <saidou@saidous-MacBook-Pro.local>
2020-02-14 05:03:44 +0000

    return list cours

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,7 @@
+"""
+
+"""
+
 from django.shortcuts import render
 from rest_framework import viewsets,status
 from .models import Course
@@ -8,20 +12,22 @@ class CourseView(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     
-    @action(detail=False, methods=['post'])
+    @action(detail=False, methods=['post'], url_path='create')
     def create_courses(self, request):
         data = request.data
         serializer = CourseSerializer(data=request.data)
         
         if serializer.is_valid():
             serializer.save()
-            return Response(data,status=status.HTTP_200_OK)
+            return Response(data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    @action(detail=True, methods=["post"])
-    def update_course(self, request):
-        pass
+    @action(detail=True, methods=["get"], url_path='list-course')
+    def list_course(self, request):
+        queryset = Course.objects.all()
+        #serializer_class = CourseSerializer
+        return Response(queryset)
     
     
 
